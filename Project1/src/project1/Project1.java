268fc10ec9e68c7f97790cdd643433b30ca7a67f
KimchiX390 <70479661+KimchiX390@users.noreply.github.com>
2020-09-23 17:30:39 -0400

    Fixed menu problem

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -9,22 +9,8 @@ public class Project1 {
     public static void main(String[] args) {
 
 
-
-    Scanner keyboard = new Scanner(System.in);
-    String help = "x";
-  
-    while (keyboard.nextLine().equalsIgnoreCase("x"))
-  {  
-    System.out.println("The game will not end until you type in the secret word.");
-    System.out.println("Navigtion: W = go forward, A = go left, S = go backwards, D = go right");
-    System.out.println("Actions: F = turn on flashlight, C = ask 'Is anyone there?' P = pick lock, E = eat food, Z = sleep");
-    System.out.println("Press 'H' for a hint.");
-    System.out.println("Warning: you will get sick after you eat, and will be forced to take a short nap in order to get better.");
-    System.out.println("Press 'Quit' to close the help menu.");
     
-  }
-    
-        System.out.println("Welcome to ASYLUM ESCAPE.");     
+    System.out.println("Welcome to ASYLUM ESCAPE.");     
     System.out.println("You were hiking in the woods and became lost. It was starting to become dark, so you"
                 + " started to look for shelter."); 
     System.out.println("You decided that you would resume trying to find help after you had gotten a good night's sleep.");
@@ -32,9 +18,25 @@ public class Project1 {
     System.out.println("Your goal is to escape from the asylum.");
     System.out.println("Press 'x' to learn how to play the game.");
 //Greeted player to game. Told them how to access help menu.
-
-    }     
-
+    
+    Scanner keyboard = new Scanner(System.in);
+    String help = "x"; 
+    String escape = "window";
+    String input = keyboard.nextLine();
+    while (! input.equalsIgnoreCase(escape)) 
+    {
+    while (input.equalsIgnoreCase(help))
+    {
+    System.out.println("The game will not end until you type in the secret word.");
+    System.out.println("Navigtion: W = go forward, A = go left, S = go backwards, D = go right");
+    System.out.println("Actions: F = turn on flashlight, C = ask 'Is anyone there?' P = pick lock, E = eat food, Z = sleep");
+    System.out.println("Press 'H' for a hint.");
+    System.out.println("Warning: you will get sick after you eat, and will be forced to take a short nap in order to get better.");
+    System.out.println("Press 'Quit' to close the help menu.");
+    input = keyboard.nextLine();
+    }
+  
+    }
  
 
     
