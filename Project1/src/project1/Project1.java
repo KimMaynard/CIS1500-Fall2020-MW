5d9b9072ddc004a59ed03f37d43b7c4c2b9947db
KimchiX390 <70479661+KimchiX390@users.noreply.github.com>
2020-09-24 19:26:05 -0400

    Completed code for game. Still need to clean it up though.

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -13,8 +13,10 @@ public class Project1 {
         System.out.println("Welcome to ASYLUM ESCAPE.");     
         System.out.println("You were hiking in the woods and became lost. It was starting to become dark, so you"
                     + " started to look for shelter."); 
-        System.out.println("You decided that you would resume trying to find help after you had gotten a good night's sleep.");
-        System.out.println("You found an abandoned asylum and decided to sleep there. As soon as you got inside, the doors shut behind you.");
+        System.out.println("You decided that you would resume trying to find help" 
+                + "after you had gotten a good night's sleep.");
+        System.out.println("You found an abandoned asylum and decided to sleep there. As soon as you got inside,"
+                + "the doors shut behind you.");
         System.out.println("Your goal is to escape from the asylum.");
         System.out.println("Press 'x' to learn how to play the game.");
 //Greeted player to game. Told them how to access help menu.
@@ -33,16 +35,31 @@ public class Project1 {
     String hint = "h";
     String sleep = "z";
     String eat = "e";
+    String word = "q";
     
-    while (! input.equalsIgnoreCase(escape)) 
+ 
+    while (! input.equalsIgnoreCase(escape))
+    { 
+    while (input.equalsIgnoreCase("q"))
     {
+        System.out.println("Type in the secret word in order to escape.");
+        input = keyboard.nextLine();
+        if (!input.equalsIgnoreCase(escape))
+        {
+            System.out.println("That's incorrect. Press 'Q' if you want to try again.");
+            input = keyboard.nextLine();
+        }
+    }
     while (input.equalsIgnoreCase(help))
     {
         System.out.println("The game will not end until you type in the secret word.");
+        System.out.println("Press 'Q' when you're ready to type in the secret word.");
         System.out.println("Navigtion: W = go forward, A = go left, S = go backwards, D = go right");
-        System.out.println("Actions: F = turn on flashlight, C = ask 'Is anyone there?' P = pick lock, E = eat food, Z = sleep");
-        System.out.println("Press 'H' for a hint.");
-        System.out.println("Warning: you will get sick after you eat, and will be forced to take a short nap in order to get better.");
+        System.out.println("Actions: F = turn on flashlight, C = ask 'Is anyone there?'" 
+                + "P = pick lock, E = eat food, Z = sleep");
+        System.out.println("Press 'H' for a hint on the secret word.");
+        System.out.println("Warning: you will get sick after you eat, and will be forced to take a" 
+                + "short nap in order to get better.");
         
         input = keyboard.nextLine();
     }
@@ -72,7 +89,7 @@ public class Project1 {
     }
     while (input.equalsIgnoreCase("c"))
     {
-        System.out.println("You call out and ask if anyone is there.");
+        System.out.println("You call out and ask if anyone is there. No one responds.");
     }
     while (input.equalsIgnoreCase("p"))
     {
@@ -95,16 +112,23 @@ public class Project1 {
         input = keyboard.nextLine();
         while (! input.equalsIgnoreCase("z"))
         {
-            System.out.println("You feel too ill to try and find a way out. You think taking a nap would probably make you feel better.");
+            System.out.println("You feel too ill to try and find a way out."
+                    + "You think taking a nap would probably make you feel better.");
             input = keyboard.nextLine();
         }
-           
     }
-    
-    
-   }
     }
-}
+   
+    if (input.equalsIgnoreCase("window"))
+    {
+        System.out.println("You climb through one of the windows and escape.");
+        System.out.println("Congratulations! You have beat the game.");
+    }
+
+
+    }}
+
+
 
                 
                 
